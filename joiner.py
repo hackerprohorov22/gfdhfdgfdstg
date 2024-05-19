d5d7e32a9c4a68c62b00c51bdf115086c49cee6a
hackerprohorov22 <63473677+hackerprohorov22@users.noreply.github.com>
2024-05-19 17:34:49 +0300

    Update joiner.py

--- joiner.py
+++ joiner.py
@@ -1,25 +1,7 @@
-"""
-
-    █▀▀ ▄▀█ █▄▀ █▀▀ █▀ ▀█▀ █░█░█ █ ▀▄▀
-    █▄▄ █▀█ █░█ ██▄ ▄█ ░█░ ▀▄▀▄▀ █ █░█
-
-    Copyleft 2022 t.me/CakesTwix
-    This program is free software; you can redistribute it and/or modify
-
-"""
-
-__version__ = (1, 0, 0)
-
-# requires: aiohttp
-# meta pic: https://www.pngall.com/wp-content/uploads/12/Avatar-Transparent.png
-# meta developer: @cakestwix_mods
-
-import logging
+from hikkatl.types import Message
 from .. import loader, utils
 import re
 import aiohttp
-from telethon import functions
-from hikkatl.types import Message
 
 logger = logging.getLogger(__name__)
 
@@ -46,19 +28,19 @@ class AutoJoinModule(loader.Module):
         self.me = await client.get_me()
         self.log_chat_id = self._db.get(self.name, "log_chat_id", None)
 
-    @loader.command(ru_doc="Начать мониторинг сообщений")
-    async def startcmd(self, message: Message):
+    @loader.command
+    async def start(self, message: Message):
         """Начать мониторинг сообщений"""
         self.is_running = True
         if not self.log_chat_id:
             await self.create_log_chat(message)
-        await self.log_to_chat(self.strings("monitoring_started"))
+        await self.log_to_chat(self.strings["monitoring_started"])
 
-    @loader.command(ru_doc="Остановить мониторинг сообщений")
-    async def stopcmd(self, message: Message):
+    @loader.command
+    async def stop(self, message: Message):
         """Остановить мониторинг сообщений"""
         self.is_running = False
-        await self.log_to_chat(self.strings("monitoring_stopped"))
+        await self.log_to_chat(self.strings["monitoring_stopped"])
 
     async def create_log_chat(self, message: Message):
         """Создать группу для логов"""
@@ -68,14 +50,14 @@ class AutoJoinModule(loader.Module):
         ))
         self.log_chat_id = result.chats[0].id
         self._db.set(self.name, "log_chat_id", self.log_chat_id)
-        await self.log_to_chat(self.strings("log_chat_created").format(title=result.chats[0].title))
+        await self.log_to_chat(self.strings["log_chat_created"].format(title=result.chats[0].title))
 
     async def log_to_chat(self, log_message: str):
         """Отправить лог в группу"""
         if self.log_chat_id:
             await self._client.send_message(self.log_chat_id, log_message)
         else:
-            logger.error("Лог-группа не создана. Пожалуйста, запустите /startcmd для создания лог-группы.")
+            logger.error("Лог-группа не создана. Пожалуйста, запустите /start для создания лог-группы.")
 
     async def watcher(self, message: Message):
         """Мониторинг сообщений"""
@@ -89,8 +71,8 @@ class AutoJoinModule(loader.Module):
                     async with aiohttp.ClientSession() as session:
                         async with session.get(link.group()) as response:
                             if response.status == 200:
-                                await self.log_to_chat(self.strings("joined_channel").format(link=link.group()))
+                                await self.log_to_chat(self.strings["joined_channel"].format(link=link.group()))
                             else:
-                                await self.log_to_chat(self.strings("failed_to_join").format(link=link.group()))
+                                await self.log_to_chat(self.strings["failed_to_join"].format(link=link.group()))
                 except Exception as e:
-                    await self.log_to_chat(self.strings("error_joining").format(error=str(e)))
+                    await self.log_to_chat(self.strings["error_joining"].format(error=str(e)))
